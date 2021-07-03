9dcd6ec8f3006c70a2ffb3d12cb9a7b398392284
afa87192 <86274180+afa87192@users.noreply.github.com>
2021-07-03 07:34:14 +0300

    Add files via upload

---
+++ untitled-2py.py
@@ -0,0 +1,17 @@
+print('''
+ ____________________________________
+|                       |            )
+|                       |          )
+|        SPAMER         |         )  
+|    by Firstov         |        )
+|      for viber        |       )
+|_______________________|_____)
+     ''')
+
+
+a=input( 'word to repeat (separated by a space)')
+f=int(input( 'number of characters with a space'))
+q= a * (7000//f )
+print(q) 
+
+
