6b0d23752b284803b71e36f2d2f2baa633d51d15
afa87192 <86274180+afa87192@users.noreply.github.com>
2021-07-10 11:47:07 +0300

    Add files via upload

--- untitled-2py.py
+++ untitled-2py.py
@@ -1,17 +1,63 @@
-print('''
- ____________________________________
-|                       |            )
-|                       |          )
-|        SPAMER         |         )  
-|    by Firstov         |        )
-|      for viber        |       )
-|_______________________|_____)
-     ''')
 
 
-a=input( 'word to repeat (separated by a space)')
-f=int(input( 'number of characters with a space'))
-q= a * (7000//f )
-print(q) 
+start= input('код команда!!!!')
+if start =='1':
+    print ('''
+    ___________________________________
+    |                       |           )
+    |                       |          )
+    |        SPAMER         |         )  
+    |    by Firstov         |        )
+    |                       |       )
+    |_______________________|______)
+    ''')
+    
+    
+    a=input( 'word to repeat (separated by a space)')
+    f=int(input( 'number of characters with a space'))
+    t=input('messenger')
+    if t=='телега':
+        t='telegram'
+    if t =='telegram' :
+        t  =  4096
+    if t =='вайбер' :
+        t  = 'viber' 
+    if t =='вк' :
+        t= 'vk'
+    if t =='vk' :
+        t  =  4096    
+    if t =='viber' :
+        t  =  7000  
+     
+    print ( a *  (t //f) ) 
+else:
 
-
+    print(''' обучение
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ''')
